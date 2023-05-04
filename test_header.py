131cc6f72f0c271b1ccad201c6489b2a0282b09f
Adam Masiarek <masiarek@gmail.com>
2023-05-04 14:04:28 -0400

    Update test_header.py

--- test_header.py
+++ test_header.py
@@ -2,8 +2,19 @@ import pytest
 
 from Header import H
 
+def test_create_header_positive():
+    # Create an H object
+    h = H(h_desc='test_desc')
 
-def test_create_header():
+    # Assert that the hdr_n attribute is incremented correctly
+    # This is first header record - we should receive the 1st number as 901
+    assert h.hdr_n == 901
+
+        # Assert that the h_desc attribute is set correctly
+    assert h.h_desc == 'test_desc'
+
+@pytest.mark.xfail
+def test_create_header_negative():
     # Create an H object
     h = H(h_desc='test_desc')
 
@@ -12,3 +23,4 @@ def test_create_header():
 
     # Assert that the h_desc attribute is set correctly
     assert h.h_desc == 'test_desc'
+
